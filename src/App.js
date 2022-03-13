6031496c2ded59f28c2531d46bac2c6e349ea32c
heopoint <heopoint@naver.com>
2022-03-13 16:49:26 +0900

    ok4

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import './App.css'
-import React, { useState, useEffect} from 'react';
-import List from './List';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import List from "./List";
 
 function App() {
+  const [todos, setTodos] = useState(["component"]);
+  const [newTodo, setnewTodo] = useState();
+  const [loading, setLoading] = useState(false);
 
-  const [todos, setTodos] = useState(['component'])
-  const [newTodo, setnewTodo] = useState()
-  
-  const changeInputData = (e) => { 
-    setnewTodo(e.target.value)
-   }
-   const addTodo = (e) => { 
-     e.preventDefault()
-     setTodos([...todos,newTodo])
-    }
-    const fetchInitialData = async() => {
-      const response= await fetch('http://localhost:5000/todo')
-      const initialData=await response.json()
-      console.log(initialData)
-      setTodos(initialData)
-    }
+  const changeInputData = (e) => {
+    setnewTodo(e.target.value);
+  };
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+  };
+  const fetchInitialData = async () => {
+    setLoading(true);
+    const response = await fetch("http://localhost:5000/todo");
+    const initialData = await response.json();
+    console.log(initialData);
+    setTodos(initialData);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetchInitialData()
-  }, [])
+    fetchInitialData();
+  }, []);
   //계속랜더링 되는걸 관찰해라
   //로링 타이밍에 한번 실행
   return (
     <div className="App">
       <h1>TODO APP</h1>
-      <form action=''>
+      <form action="">
         <input type="text" name="" onChange={changeInputData}></input>
         <button onClick={addTodo}>할일추가</button>
       </form>
-      <List todos={todos}/>
+      <List todos={todos} loading={loading} />
     </div>
   );
 }
--- src/List.js
+++ src/List.js
@@ -1,12 +1,9 @@
-import React from 'react'
+import React from "react";
 
-function List({todos}) {
- 
-  return (
-    <ul>
-    {todos.map(todo=><li>{todo.title}</li>)}
-    </ul>
-  )
+function List({ todos, loading }) {
+  let todoList = <div>Loading....</div>;
+  if (!loading) {todoList = todos.map((list) => <li key={list.id}>{list.title}</li>)};
+  return <ul>{todoList}</ul>;
 }
 
-export default List
+export default List;
