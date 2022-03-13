3776e09ca82f143d02ab5d88d5851cd1a83a4ef2
heopoint <heopoint@naver.com>
2022-03-13 16:17:31 +0900

    ok2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { useState} from 'react';
-import List from './List';
+import React, { useState } from "react";
+import List from "./List";
 
 function App() {
+  const [todos, setTodos] = useState(["component"]);
+  const [newTodo, setnewTodo] = useState();
 
-  const [todos, setTodos] = useState(['component'])
- 
+  const changeInputData = (e) => {
+    setnewTodo(e.target.value);
+  };
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+  };
 
   return (
     <div className="App">
       <h1>TODO APP</h1>
-      <form action=''>
-        <input type="text" name="" ></input>
-        <button>명령어추가</button>
+      <form action="">
+        <input type="text" name="" onChange={changeInputData}></input>
+        <button onClick={addTodo}>할일추가</button>
       </form>
-      <List todos={todos}/>
+      <List todos={todos} />
     </div>
   );
 }
